cd863eb9ce93ed25673c434599eb2134a2bc969e
PebbleYYC <harp3r.ian@gmail.com>
2026-04-28 21:09:10 -0600

    Folder Organization

---
+++ build/diorite/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "BrutalTimeStyle",
+  .company = "Freakifiedxir33kxPebbleYYC",
+  .icon_resource_id = RESOURCE_ID_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE | PROCESS_INFO_PLATFORM_DIORITE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x10, 0xC5, 0x99, 0x59, 0x7A, 0xD9, 0x49, 0x8C, 0xB9, 0xE6, 0xE2, 0x88, 0x8F, 0x49, 0xF9, 0xB9 },
+  .virtual_size = 0xb6b6
+};
